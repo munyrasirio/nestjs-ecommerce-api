31a07e59c3e6e1831c3603bcadbb9b050053adc3
Munyra Sírio <munyrasirio@gmail.com>
2023-05-27 23:49:51 -0300

    feat: product validation

---
+++ src/product/dto/createProduct.dto.ts
@@ -0,0 +1,68 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsUrl,
+  MaxLength,
+  Min,
+  ValidateNested,
+} from 'class-validator';
+
+export class ProductCharacteristicsDTO {
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  @MaxLength(1000)
+  description: string;
+}
+
+export class ProductImagesDTO {
+  @IsUrl()
+  url: string;
+
+  @IsNotEmpty()
+  @MaxLength(1000)
+  description: string;
+}
+
+export class CreateProductDTO {
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsPositive()
+  price: number;
+
+  @IsNumber()
+  @Min(0)
+  quantity: number;
+
+  @IsNotEmpty()
+  @MaxLength(1000)
+  description: string;
+
+  @IsNotEmpty()
+  category: string;
+
+  @IsArray()
+  @ArrayMinSize(3)
+  @ValidateNested()
+  @Type(() => ProductCharacteristicsDTO)
+  characteristics: ProductCharacteristicsDTO[];
+
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested()
+  @Type(() => ProductImagesDTO)
+  images: ProductImagesDTO[];
+
+  @IsNotEmpty()
+  created_at: string;
+
+  @IsNotEmpty()
+  updated_at: string;
+}
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProductRepository } from './product.repository';
+import { CreateProductDTO } from './dto/createProduct.dto';
 
 @Controller('/products')
 export class ProductController {
   constructor(private productRepository: ProductRepository) {}
 
   @Post()
-  async createProduct(@Body() product) {
+  async createProduct(@Body() product: CreateProductDTO) {
     this.productRepository.save(product);
 
     return product;
