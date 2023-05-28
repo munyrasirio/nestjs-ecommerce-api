e3eb17976e08b04febeba5d3061b6ecbcb4c104a
Munyra Sírio <munyrasirio@gmail.com>
2023-05-28 15:37:43 -0300

    feat: updated product module with type and dto

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,16 +1,25 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProductRepository } from './product.repository';
 import { CreateProductDTO } from './dto/create-product.dto';
+import { UUID, randomUUID } from 'crypto';
+import { TProduct } from './product.type';
+import { GetProductDTO } from './dto/get-product.dto';
 
 @Controller('/products')
 export class ProductController {
   constructor(private productRepository: ProductRepository) {}
 
   @Post()
-  async createProduct(@Body() product: CreateProductDTO) {
+  async createProduct(@Body() productData: CreateProductDTO) {
+    const id = randomUUID();
+    const product: TProduct = { ...productData, id };
     this.productRepository.save(product);
 
-    return product;
+    return {
+      product: new GetProductDTO(product.id, product.userId, product.name),
+      message: 'Product created successfully.',
+    };
+  }
   }
 
   @Get()
--- src/product/product.repository.ts
+++ src/product/product.repository.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { TProduct } from './product.type';
+import { UUID } from 'crypto';
 
 @Injectable()
 export class ProductRepository {
-  private products = [];
+  private products: TProduct[] = [];
 
-  async save(product) {
+  async save(product: TProduct) {
     this.products.push(product);
   }
 
