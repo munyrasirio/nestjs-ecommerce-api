5432097e57cfc388203adea5b6981d86419ccb57
Munyra Sírio <munyrasirio@gmail.com>
2023-05-27 23:49:08 -0300

    feat: user validation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserRepository } from './user.repository';
+import { CreateUserDTO } from './dto/createUser.dto';
 
 @Controller('/users')
 export class UserController {
   constructor(private userRepository: UserRepository) {}
 
   @Post()
-  async createUser(@Body() userData) {
+  async createUser(@Body() userData: CreateUserDTO) {
     this.userRepository.save(userData);
 
     return userData;
